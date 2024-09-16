87c3e7c753664da248bb81a911b138fe31bd59b6
sherlyazkiah <sherlyazkiah12@gmail.com>
2024-09-16 11:44:40 +0700

    modify the loan amount and installments so that it can receive input

--- WEEK3/Task/Test.java
+++ WEEK3/Task/Test.java
@@ -1,14 +1,26 @@
 package WEEK3.Task;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
         Member donny = new Member("111333444", "Donny", 5000000);
 
         System.out.println("Member name: " + donny.getName());
         System.out.println("Loan Limit: " + donny.getLoanLimit());
 
-        System.out.println("\nBorrow money 10.000.000");
+        System.out.print("\nEnter amount to borrow: ");
+        float borrowAmount = sc.nextFloat();
+        donny.borrow(borrowAmount);
+
+        System.out.print("\nEnter pay installments: ");
+        float installment = sc.nextFloat();
+        donny.installments(installment);
+        System.out.println("Current loan amount: " + donny.getLoanAmount());
+
+        /*System.out.println("\nBorrow money 10.000.000");
         donny.borrow(10000000);
         System.out.println("Current loan amount: " + donny.getLoanAmount());
 
@@ -26,6 +38,8 @@ public class Test {
 
         System.out.println("\nPay installments 3.000.000");
         donny. installments (3000000);
-        System.out.println("Current loan amount: " + donny.getLoanAmount ());
+        System.out.println("Current loan amount: " + donny.getLoanAmount ());*/
+
+        sc.close();
     }
 }
