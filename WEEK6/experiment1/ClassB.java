b2dbef69e935be3061dce9070cc396dc2718e42e
sherlyazkiah <sherlyazkiah12@gmail.com>
2024-10-04 09:37:16 +0700

    initial commit

---
+++ WEEK6/experiment1/ClassB.java
@@ -0,0 +1,13 @@
+package experiment1;
+
+public class ClassB extends ClassA{
+    public int z;
+
+    public void getNilaiZ() {
+        System.out.println("nilai z: " + z);
+    }
+
+    public void getJumlah() {
+        System.out.println("jumlah: " + (x+y+z));
+    }
+}
